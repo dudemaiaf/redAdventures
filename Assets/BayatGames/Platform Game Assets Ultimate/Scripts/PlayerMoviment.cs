6e130cfb58f0b9b76e9103841fdcc2a54ddb8772
Eduardo Maia Freire <emf.snf16@uea.edu.br>
2019-12-11 21:22:27 -0400

    feat: movimentacao e começo de animação

---
+++ Assets/BayatGames/Platform Game Assets Ultimate/Scripts/PlayerMoviment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{   
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        StartCoroutine(settingRunAnimation());
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        //if(Input.GetButtonDown)
+    }
+
+    IEnumerator settingRunAnimation (){
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Animação 2");
+    }
+
+    void FixedUpdate ()
+    {
+        //move o player
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
